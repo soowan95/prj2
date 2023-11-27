c6c8b67fd334dcf30f9ff6f7fede3f845bed9b71
jjeongneee <gardening0530@gmail.com>
2023-11-27 11:38:58 +0900

    보안 질문 추가 및 수정 중.

--- src/page/member/MemberSignup.js
+++ src/page/member/MemberSignup.js
@@ -20,6 +20,14 @@ import { useState } from "react";
 import axios from "axios";
 
 export function MemberSignup() {
+  // securityQuestionList를 함수 외부에서 선언
+  let securityQuestionList = [
+    "가장 좋아하는 색은 무엇입니까?",
+    "가장 좋아하는 영화 제목은 무엇입니까?",
+    "처음으로 가보았던 해변의 이름은 무엇입니까?",
+    "가장 처음 가본 콘서트는 어떤 가수의 콘서트였습니까?",
+  ];
+
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
   const [passwordCheck, setPasswordCheck] = useState("");
@@ -27,8 +35,10 @@ export function MemberSignup() {
   const [nickName, setNickName] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const [sequrityQuestion, setSequrityQuestion] = useState(0);
-  const [sequrityAnswer, setSequrityAnswer] = useState("");
+  const [selectedSecurityQuestion, setSecurityQuestion] = useState(
+    securityQuestionList[0],
+  );
+  const [securityAnswer, setSecurityAnswer] = useState("");
 
   const [idAvailable, setIdAvailable] = useState(false);
 
@@ -55,11 +65,30 @@ export function MemberSignup() {
         password,
         nickName,
         email,
-        sequrityQuestion,
-        sequrityAnswer,
+        securityQuestion: selectedSecurityQuestion, // 여기서 selectedSecurityQuestion을 사용
+        securityAnswer,
       })
-      .then(() => console.log("good"))
-      .catch(() => console.log("bad"))
+      .then(() => {
+        toast({
+          title: "가입 완료",
+          description: "회원 가입이 성공적으로 완료되었습니다.",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+
+        // 가입이 완료되면 모달을 닫음
+        setIsModalOpen(false);
+      })
+      .catch(() => {
+        toast({
+          title: "가입 실패",
+          description: "회원 가입 중 오류가 발생했습니다.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      })
       .finally(() => console.log("done"));
   }
 
@@ -89,12 +118,39 @@ export function MemberSignup() {
       });
   }
 
-  const securityQuestion = [
-    "가장 좋아하는 색은 무엇입니까?",
-    "가장 좋아하는 영화 제목은 무엇입니까?",
-    "처음으로 가보았던 해변의 이름은 무엇입니까?",
-    "가장 처음 가본 콘서트는 어떤 가수의 콘서트였습니까?",
-  ];
+  function handleForgotPassword() {
+    // 사용자에게 입력받은 ID를 서버로 전송하여 보안 질문을 가져옴
+    axios
+      .get(`/api/member/securityQuestion/${id}`)
+      .then((response) => {
+        const serverSecurityQuestion = response.data.sequrityQuestion;
+
+        // 사용자에게 보안 질문 물어보고 답변 확인
+        const userAnswer = prompt(serverSecurityQuestion);
+        if (userAnswer === null) {
+          return;
+        }
+
+        // 서버로 보안 질문 답변을 전송하여 확인
+        axios
+          .post("/api/member/forgotPassword", {
+            id,
+            securityAnswer: userAnswer,
+          })
+          .then(() => {
+            // 비밀번호 재설정 성공
+            alert("비밀번호가 성공적으로 재설정되었습니다.");
+          })
+          .catch(() => {
+            // 보안 질문 답변 일치하지 않을 경우
+            alert("답변이 올바르지 않습니다.");
+          });
+      })
+      .catch(() => {
+        // ID에 해당하는 보안 질문을 찾지 못했을 때
+        alert("ID에 해당하는 보안 질문을 찾지 못했습니다.");
+      });
+  }
 
   return (
     <Box>
@@ -157,25 +213,32 @@ export function MemberSignup() {
             <FormControl mb={5}>
               <FormLabel>보안 질문</FormLabel>
               <Select
-                value={securityQuestion}
-                onChange={(e) => setSequrityAnswer(parseInt(e.target.value))}
+                value={selectedSecurityQuestion}
+                onChange={(e) => setSecurityQuestion(e.target.value)}
               >
-                {sequrityQuestion.map((question, index) => (
-                  <option key={index} value={index}>
+                {securityQuestionList.map((question, index) => (
+                  <option key={index} value={question}>
                     {question}
                   </option>
                 ))}
               </Select>
+              <Input
+                type="text"
+                value={securityAnswer}
+                onChange={(e) => setSecurityAnswer(e.target.value)}
+              />
             </FormControl>
           </ModalBody>
           <ModalFooter>
             <Button
               isDisabled={!submitAvailable}
               onClick={handelSubmit}
-              colorScheme="purple.200"
+              colorScheme="purple"
             >
               가입
             </Button>
+
+            <Button onClick={handleForgotPassword}>비밀번호 찾기</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
