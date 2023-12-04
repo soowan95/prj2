019f4e3ca5d30d2d9700c90ead0812927a1da17d
1082cubi <1082cubi@hanmail.net>
2023-12-04 11:13:26 +0900

    Suggestion 다시 입력

---
+++ src/page/main/ChartPage.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import axios from "axios";
+
+export function ChartPage() {
+  const [songList, setSongList] = useState(null);
+  useEffect(() => {
+    axios
+      .get("/api/song/chartlist")
+      .then((response) => setSongList(response.data))
+      .catch(() => console.log("bad"));
+  }, []);
+  return (
+    <Box>
+      <h1> 게시물 목록 </h1>
+      <Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>번호</Th>
+              <Th></Th>
+              <Th>곡정보</Th>
+              <Th></Th>
+              <Th>듣기</Th>
+              <Th>추가</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {songList === null ? (
+              <spinner />
+            ) : (
+              songList.map((song) => (
+                <Tr>
+                  <Td>{song.id}</Td>
+                  <Td>{song.title}</Td>
+                  <Td>{song.artistName}</Td>
+                  <Td>{song.album}</Td>
+                  <Td>{song.mood}</Td>
+                </Tr>
+              ))
+            )}
+          </Tbody>
+        </Table>
+      </Box>
+    </Box>
+  );
+}
+
+export default ChartPage;
