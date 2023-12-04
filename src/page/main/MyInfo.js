7da05b4edf3dd18c3880320b699da58035bd2854
hoonil <phi0588@naver.com>
2023-12-04 10:58:40 +0900

    hoonil front1204

--- src/page/main/MyInfo.js
+++ src/page/main/MyInfo.js
@@ -17,36 +17,44 @@ import { useNavigate } from "react-router-dom";
 import { LoginContext } from "../../component/LoginProvider";
 
 export function MyInfo() {
-  const [login, setLogin] = useState("");
+  const {login} = useContext(LoginContext);
+  const [loginInfo, setLoginInfo] = useState(null);
 
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   axios.get("/api/member/logininfo").then(({ data }) => setLogin(data));
-  // }, []);
+  useEffect(() => {
+    axios.get("/api/member/logininfo")
+        .then((response) => setLoginInfo(response.data))
+  }, []);
   return (
-    <Popover>
-      <PopoverTrigger>
-        <Button fontSize={"1.7rem"}>
-          <FontAwesomeIcon icon={faUser} />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent>
-        <PopoverArrow />
-        <PopoverCloseButton />
-        <PopoverHeader>
-          {login.nickName} 님
-          <Button size="xs" ml={5}>
-            LOGOUT
+      <Popover>
+        <PopoverTrigger>
+          <Button fontSize={"1.7rem"}>
+            <FontAwesomeIcon icon={faUser}/>
           </Button>
-        </PopoverHeader>
-        <PopoverBody>
-          <Button onClick={() => navigate("/myplaylist")} variant="ghost">
-            MyPlayList
-          </Button>
-        </PopoverBody>
-        <PopoverFooter>Logout Button</PopoverFooter>
-      </PopoverContent>
-    </Popover>
+        </PopoverTrigger>
+        <PopoverContent>
+          <PopoverArrow/>
+          <PopoverCloseButton/>
+          <PopoverHeader>
+            {loginInfo ? (
+                <>
+                  {loginInfo.nickName} 님 환영합니다
+                </>
+            ) : (
+                "로딩 중..." // 또는 다른 로딩 처리 방식을 사용할 수 있습니다.
+            )}
+          </PopoverHeader>
+          <PopoverBody>
+            <Button variant="ghost" onClick={()=>navigate("/main/recommended")}>추천 플레이리스트</Button><br/>
+            <Button onClick={() => navigate("/main/myplaylist")} variant="ghost">
+              나의 재생목록
+            </Button> <br/>
+            <Button variant="ghost">나의 좋아요 목록</Button><br/>
+            <Button variant="ghost">내 정보 수정</Button>
+          </PopoverBody>
+          <PopoverFooter>로그아웃</PopoverFooter>
+        </PopoverContent>
+      </Popover>
   );
 }
--- src/page/main/MyPlayList.js
+++ src/page/main/MyPlayList.js
@@ -1,81 +1,79 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {
-  Box,
-  Button,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Center,
-  Divider,
-  Flex,
-  Heading,
-  Image,
-  Spacer,
-  Text,
+    Box,
+    Button,
+    Card,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    Center,
+    Divider,
+    Flex,
+    Heading,
+    Image,
+    Spacer,
+    Text,
 } from "@chakra-ui/react";
 import axios from "axios";
-import { useLocation, useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRecordVinyl } from "@fortawesome/free-solid-svg-icons";
-import { LoginContext } from "../../component/LoginProvider";
+import {useLocation, useNavigate} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faRecordVinyl} from "@fortawesome/free-solid-svg-icons";
+import {LoginContext} from "../../component/LoginProvider";
 
 export function MyPlayList() {
-  const navigate = useNavigate();
-  const [list, setList] = useState(null);
+    const navigate = useNavigate();
+    const [list, setList] = useState(null);
 
-  // const { login } = useContext(LoginContext);
-  const location = useLocation();
+    const {login} = useContext(LoginContext);
+    const location = useLocation();
 
-  // useEffect(() => {
-  //   const params = new URLSearchParams();
-  //   params.set("listId", login.id);
-  //   axios.get("/api/myList/get?" + params).then(({ data }) => setList(data));
-  // }, [location]);
+    useEffect(() => {
+        const params = new URLSearchParams();
+        params.set("listId", login.id);
+        axios.get("/main/api/myList/get?" + params).then(({data}) => setList(data));
+    }, [location]);
 
-  function handlePlaylist() {}
 
-  return (
-    <>
-      <Divider />
-      <Heading>asdfasdf{/*{login.id} 님의 재생목록*/}</Heading>
-      <Divider />
-      <Flex gap={5}>
-        {list !== null &&
-          list.map((song) => (
-            <Box gap={5} key={song.id}>
-              <Box mt={30}>
-                <Card w="xs">
-                  <CardHeader
-                    _hover={{ cursor: "pointer" }}
-                    onClick={handlePlaylist}
-                  >
-                    <Image src="https://cdn.dribbble.com/users/5783048/screenshots/13902636/skull_doodle_4x.jpg" />
-                  </CardHeader>
-                  <CardBody>
-                    <Heading
-                      size="md"
-                      _hover={{
-                        cursor: "pointer",
-                        textDecoration: "underline",
-                      }}
-                      onClick={handlePlaylist}
-                    >
-                      {song.listName}
-                    </Heading>
-                  </CardBody>
-                  <Divider color="gray" />
-                  <CardFooter>
-                    <FontAwesomeIcon icon={faRecordVinyl} />
-                    <Text>$곡</Text>
-                    <Spacer />
-                    <Text>생성일 : </Text>
-                  </CardFooter>
-                </Card>
-              </Box>
-            </Box>
-          ))}
-      </Flex>
-    </>
-  );
+    return (
+        <>
+            <Divider/>
+            <Heading>{login.nickName} 님의 재생목록</Heading>
+            <Divider/>
+            <Flex gap={5}>
+                {list !== null &&
+                    list.map((song) => (
+                        <Box gap={5} key={song?.id}>
+                            <Box mt={30}>
+                                <Card w="xs">
+                                    <CardHeader
+                                        _hover={{cursor: "pointer"}}
+                                    >
+                                        <Image
+                                            src="https://cdn.dribbble.com/users/5783048/screenshots/13902636/skull_doodle_4x.jpg"/>
+                                    </CardHeader>
+                                    <CardBody>
+                                        <Heading
+                                            size="md"
+                                            _hover={{
+                                                cursor: "pointer",
+                                                textDecoration: "underline",
+                                            }}
+                                        >
+                                            {song?.listName}
+                                        </Heading>
+                                    </CardBody>
+                                    <Divider color="gray"/>
+                                    <CardFooter>
+                                        <FontAwesomeIcon icon={faRecordVinyl}/>
+                                        <Text>$곡</Text>
+                                        <Spacer/>
+                                        <Text>생성일 : </Text>
+                                    </CardFooter>
+                                </Card>
+                            </Box>
+                        </Box>
+                    ))}
+            </Flex>
+        </>
+    );
 }
