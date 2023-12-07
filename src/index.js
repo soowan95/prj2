9cd7afc42c0a9928e5884c4ee91d6b5c7e04adc2
soowan95 <ksw9565@gmail.com>
2023-12-07 16:05:04 +0900

    bug fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { createRoot } from "react-dom/client";
-import { Helmet } from "react-helmet";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
