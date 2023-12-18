dff6fc9fa88201d95f8ccee9adcd17ec05bc8476
jjo2023 <snowman7685@gmail.com>
2023-12-18 12:41:37 +0900

    mj css/search

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom"; // ReactDOM 추가
 import App from "./App";
-import { ChakraProvider, CSSReset, extendTheme } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  ColorModeScript,
+  CSSReset,
+  extendTheme,
+} from "@chakra-ui/react";
 import "./css/Fonts.css";
 
 // 원하는 글꼴 패밀리로 사용자 정의 테마 정의
@@ -10,11 +15,17 @@ const theme = extendTheme({
     heading: "SUITE-Regular",
     body: "SUITE-Regular",
   },
+
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
 });
 
 // createRoot 대신 ReactDOM.render 사용
 ReactDOM.render(
   <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <CSSReset />
     <App />
   </ChakraProvider>,
